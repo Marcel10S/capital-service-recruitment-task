a093386eeb795a7b73577f334e24fb04f16fc326
Marcel10S <marcelstulczewski@gmail.com>
2024-12-03 16:51:36 +0100

    Handle docker basics

---
+++ src/Entity/Loan.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LoanRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LoanRepository::class)]
+class Loan
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $amount;
+
+    #[ORM\Column(type: 'integer')]
+    private $installments;
+
+    #[ORM\Column(type: 'float')]
+    private $interestRate;
+
+    #[ORM\Column(type: 'datetime')]
+    private $createdAt;
+
+    #[ORM\Column(type: 'boolean', options: ["default" => false])]
+    private $excluded = false;
+}
